59a54c7d5566229e0fe8fad78a5d15041481b4e7
Artghochikyan <ttgoch@gmail.com>
2023-09-26 20:43:34 +0400

    my16

--- Lessons-1/create.sh
+++ Lessons-1/create.sh
@@ -4,5 +4,4 @@ terraform apply
 echo "Fetching ip from output"
 terraform output | awk -F'"' '/"/ {print $2}' > inventory
 echo "Running ansible"
-cd ansible
 ansible-playbook -i inventory ansible-wordpress.yaml
