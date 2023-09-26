ac6751754c13ab8add39e07e21575080100f1f77
Artghochikyan <ttgoch@gmail.com>
2023-09-26 20:57:56 +0400

    my16

--- Lessons-1/create.sh
+++ Lessons-1/create.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 echo "Running terraform"
+terraform init
 terraform apply
 echo "Fetching ip from output"
 terraform output | awk -F'"' '/"/ {print $2}' > inventory
-echo "Running ansible"
+echo "Running ansible-wordpress.yaml"
 ansible-playbook -i inventory ansible-wordpress.yaml
