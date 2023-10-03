924408e6e3ab1cd63b7d1a98639a4c106f62fa94
Artghochikyan <ttgoch@gmail.com>
2023-10-03 20:09:47 +0400

    changes3

--- Lessons-1/create.sh
+++ Lessons-1/create.sh
@@ -4,4 +4,4 @@ terraform init && terraform apply -auto-approve -lock=false
 echo "Fetching ip from output"
 terraform output | awk -F'"' '/"/ {print $2}' > inventory
 echo "Running ansible-wordpress.yaml"
-ansible-playbook -i inventory ansible-wordpress.yaml -e mysql_host=$mysql_host -e wp_pass=${{secrets.WORDPRESS_DB_PASSWORD}} --ssh-common-args='-o StrictHostKeyChecking=no'
+ansible-playbook -i inventory ansible-wordpress.yaml #-e mysql_host=$mysql_host -e wp_pass=${{secrets.WORDPRESS_DB_PASSWORD}} --ssh-common-args='-o Strict
